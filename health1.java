ca4e0f3e72f932cda74868f5105b7a8d2bc12852
tolapisaichandana <165548386+tolapisaichandana@users.noreply.github.com>
2025-04-12 20:55:42 +0530

    Update health1.java

--- health1.java
+++ health1.java
@@ -3,5 +3,7 @@ class A
     public static void main(string[] args)
     {
       Sytem.out.println("Welcome ");
+      System.out.println("hello world");
+      System.out.println("GIT PRactice");
         }
   }
